ad03543b2256a6be031dc5a49a5a0e8af1f84be9
Rohit T P <tprohit9@gmail.com>
2022-08-10 20:12:40 +0530

    websockets added

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from asyncio import sleep
+from typing import List
+
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from starlette import status
 from starlette.middleware.cors import CORSMiddleware
 from starlette import responses
+from starlette.websockets import WebSocket, WebSocketDisconnect
 
 from google_auth import auth
 from google_auth.db import get_db, User
@@ -27,8 +31,39 @@ app.add_middleware(
 app.include_router(auth.router)
 
 
-def unlock_door():
-    pass
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+async def unlock_door():
+    await manager.broadcast("OPEN")
+    await sleep(3)
+    await manager.broadcast("CLOSE")
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            print(await websocket.receive_text())
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
 
 
 @app.get("/award")
@@ -52,14 +87,14 @@ def add_unlocks(email: str, unlocks: int,user: User = Depends(get_current_user))
 
 
 @app.get("/")
-def home(user: User | None = Depends(get_current_user)):
+async def home(user: User | None = Depends(get_current_user)):
     if not user:
         return responses.RedirectResponse(url="/google_login_client",
                                           status_code=status.HTTP_307_TEMPORARY_REDIRECT)
 
-    if user.max_unlock <= 0 and not user.is_admin:
-        return responses.PlainTextResponse(content="Maximum unlock limit reached",
-                                           status_code=status.HTTP_400_BAD_REQUEST)
+    # if user.max_unlock <= 0 and not user.is_admin:
+    #     return responses.PlainTextResponse(content="Maximum unlock limit reached",
+    #                                        status_code=status.HTTP_400_BAD_REQUEST)
 
     if not user.is_admin:
         db: Session = next(get_db())
@@ -70,6 +105,6 @@ def home(user: User | None = Depends(get_current_user)):
         db.commit()
         db.refresh(user)
 
-    unlock_door()
+    await unlock_door()
 
     return responses.PlainTextResponse(content=f"Door unlocked, {user.max_unlock} unlocks remaining")
